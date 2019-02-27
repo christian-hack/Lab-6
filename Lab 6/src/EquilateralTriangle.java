641efa58ee275aa9b694a5fa703d08e25e7cb46b
Christian Hackelman <cphackelman@ou.edu>
2019-02-26 19:35:50 -0600

    More classes finished

---
+++ Lab 6/src/EquilateralTriangle.java
@@ -0,0 +1,53 @@
+/**
+ * Lab 6
+ *
+ * Class representing an equilateral Triangle.
+ *
+ * @author Stephen
+ * @version 2019-02-18
+ */
+public class EquilateralTriangle extends Polygon
+{
+    /**
+     * Length of the sides of the equilateral triangle.
+     */
+    private double sideLength;
+
+	/**
+	 * Constructor for the equilateral triangle. Sets sideLength member variable and sets
+	 * all side length in Polygon sidelengths array.
+	 *
+	 * @param sidelength The length of the triangle sides.
+	 */
+	public EquilateralTriangle(String id, double sideLength)
+	{
+		super(id);
+		this.sideLength = sideLength;
+		sideLengths.add(sideLength);
+		sideLengths.add(sideLength);
+		sideLengths.add(sideLength);
+	}
+
+	/**
+	 * Gets the area of the triangle.
+	 *
+	 * @return The area of the triangle (b*h/2 = sidelength*sidelength*root(3)/4)
+	 */
+	@Override
+	public double getArea()
+	{
+		double triArea = sideLength * Math.sqrt(.75);
+		return triArea;
+	}
+
+	/**
+	 * Gets the shape type of the triangle.
+	 *
+	 * @return The string "EquilateralTriangle"
+	 */
+	@Override
+	public String getShapeType()
+	{
+		return "EquilateralTriangle";
+	}
+}
