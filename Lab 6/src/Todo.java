4dd632081a22dc3a7741290a6d274c7ec6dad306
Christian Hackelman <cphackelman@ou.edu>
2019-02-27 23:19:47 -0600

    Uploading todo class

---
+++ Lab 6/src/Todo.java
@@ -0,0 +1,23 @@
+
+public class Todo { /*
+Lab Section_________: 11
+Last Modified_______: 2019-02-26
+OU ID_______________: 113075103
+OU EMAIL____________: cphackelman@ou.edu
+
+Tasks:
+1. Complete all todo's in claasses excluding tests/comparators
+2. Complete comparator classes
+3. Complete tests
+
+Estimated Time (minutes):
+1. 45
+2. 45
+3. 30
+
+Actual Time (minutes):
+1. 90
+2. 30
+3. 45
+*/
+}
